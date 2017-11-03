21c6d51f3e3ed2e31e7b33d3392efe1c1aa0a81d
sachinverma <v.sachin.v@gmail.com>
2017-11-03 14:44:12 +0530

    Added Support for OkHttpSender to make Ratpack Zipkin Example workable with Zipkin Server 2.1.x

--- src/main/java/com/example/server/App.java
+++ src/main/java/com/example/server/App.java
@@ -8,8 +8,10 @@ import ratpack.zipkin.ServerTracingModule;
 import zipkin.Span;
 import zipkin.reporter.AsyncReporter;
 import zipkin.reporter.Reporter;
+import zipkin.reporter.Sender;
 import zipkin.reporter.kafka08.KafkaSender;
 import zipkin.reporter.libthrift.LibthriftSender;
+import zipkin.reporter.okhttp3.OkHttpSender;
 
 /**
  * RatPack Server.
@@ -51,22 +53,27 @@ public class App {
     );
   }
 
-  private static Reporter<Span> spanReporter() {
-    String scribeHost = System.getProperty("scribeHost");
-    String kafkaHost = System.getProperty("kafkaHost");
-    if (scribeHost != null) {
-      return AsyncReporter.builder(LibthriftSender.builder()
-                                           .host(scribeHost)
-                                           .port(9410)
-                                           .build()).build();
-    } else if (kafkaHost != null) {
-      return AsyncReporter.builder(KafkaSender.builder()
-          .bootstrapServers(kafkaHost)
-          .topic("zipkin")
-          .build()).build();
-    } else {
-      return Reporter.CONSOLE;
+    private static Reporter<Span> spanReporter() {
+        String scribeHost = System.getProperty("scribeHost");
+        String kafkaHost = System.getProperty("kafkaHost");
+        String okHttpSenderEndPoint = System.getProperty("okHttpSenderEndPoint"); //http://localhost:9411/api/v1/spans
+        if (scribeHost != null) {
+            return AsyncReporter.builder(LibthriftSender.builder()
+                    .host(scribeHost)
+                    .port(9410)
+                    .build()).build();
+        } else if (kafkaHost != null) {
+            return AsyncReporter.builder(KafkaSender.builder()
+                    .bootstrapServers(kafkaHost)
+                    .topic("zipkin")
+                    .build()).build();
+        } else if (okHttpSenderEndPoint != null) {
+            Sender sender = OkHttpSender.builder().endpoint(okHttpSenderEndPoint).build();
+            return AsyncReporter.create(sender);
+
+        } else {
+            return Reporter.CONSOLE;
+        }
     }
-  }
 
 }
